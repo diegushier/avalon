7bc1246bc537abf07f1980c4e7fe93b438323404
Diego Hilderink Dominguez <diego.hilderink@iesdonana.org>
2020-05-16 11:05:23 +0200

    [MOD] Limpio código de valoraciones y criticas

--- controllers/CriticasController.php
+++ controllers/CriticasController.php
@@ -30,34 +30,6 @@ class CriticasController extends Controller
     }
 
     /**
-     * Lists all Criticas models.
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        $searchModel = new CriticasSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
-    }
-
-    /**
-     * Displays a single Criticas model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
      * Creates a new Criticas model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
@@ -71,26 +43,6 @@ class CriticasController extends Controller
         }
 
         return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Updates an existing Criticas model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
             'model' => $model,
         ]);
     }
--- controllers/ValoracionesController.php
+++ controllers/ValoracionesController.php
@@ -30,19 +30,6 @@ class ValoracionesController extends Controller
     }
 
     /**
-     * Displays a single Valoraciones model.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
      * Creates a new Valoraciones model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
@@ -56,26 +43,6 @@ class ValoracionesController extends Controller
         }
 
         return $this->render('create', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Updates an existing Valoraciones model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
             'model' => $model,
         ]);
     }
